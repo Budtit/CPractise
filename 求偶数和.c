04a070ea1142b8c8e27a6c58a3295f323f8599d3
budtit <z734110414z@163.com>
2021-05-15 21:09:12 +0800

    first

--- 求偶数和.c
+++ 求偶数和.c
@@ -3,4 +3,17 @@
 //
 /*编制程序，输入n个整数（n从键盘输入，n>0），输出它们的偶数和。*/
 
+#include "stdio.h"
 
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b[a];
+    for (int i = 0; i <= a; i++) {
+        scanf("%d",&a[i]);
+    }
+    for(i = 0; i<=a;i++){
+        printf("%d",a[i])
+    }
+    return 0;
+}
